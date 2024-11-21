b23721b9b3dd974d2a6040641caf445aa301b154
Huy180105 <huy74dcht21108@st.utt.edu.vn>
2024-11-21 20:57:08 +0700

    lythuyetcss

---
+++ baitapjs.js
@@ -0,0 +1,16 @@
+/* viết 1 hàm trong file main.js tạo 1 biến lưu danh sách a nlớp gồm 9 phần tử, là thành viên củhóm mình các thông tin họ và tên, giới tính, nơi sinh. Dùng javacript để in danh sách đó ra */
+function indanhsach() {
+       const thanhvien =[
+        {hovaten: 'Nguyễn Quang Huy', gioitinh: 'Nam', Noisinh: 'Hà Tĩnh' },
+        {hovaten: 'Trần Đình Hưng Thịnh', gioitinh: 'Nam', Noisinh: 'Nghệ An'},
+        {hovaten: 'Trần Minh Long', gioitinh: 'Nam', Noisinh: 'Hà Nội'},
+        {hovaten: 'Đào Thế Vũ', gioitinh: 'Nam', Noisinh: 'Hà Nội'},
+        {hovaten: 'Nguyễn Công Mạnh', gioitinh: 'Nam', Noisinh: 'Hà Tĩnh'},
+        {hovaten: 'Nguyễn Nam Khánh', gioitinh: 'Nam', Noisinh: 'Hà Nội'},
+        {hovaten: 'Nguyễn Việt Dương', gioitinh: 'Nam', Noisinh: 'Hà Nội'},
+        {hovaten: 'Trần Thị Thu Huyền', gioitinh: 'Nữ', Noisinh: 'Hà Nội'},
+        {hovaten: 'Đỗ Thị Lan Hương', gioitinh: 'Nữ', Noisinh: 'Hải Phòng'},
+           ];
+           console.log ('danhsach',thanhvien)
+}
+indanhsach();
